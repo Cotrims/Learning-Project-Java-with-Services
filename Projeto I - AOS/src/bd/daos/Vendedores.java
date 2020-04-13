a1e999eef7fb80035945e9883b96230bbbcfb7e1
Vinícius Cotrim <vini.cotrim@hotmail.com>
2020-04-13 01:16:16 -0300

    Desenvolvimento da janela Integração do uso de Web Service Finalização nos comando e ações da Janela

---
+++ Projeto I - AOS/src/bd/daos/Vendedores.java
@@ -0,0 +1,245 @@
+package bd.daos;
+
+import java.sql.*;
+import bd.*;
+import bd.dbos.*;
+import bd.core.MeuResultSet;
+
+public class Vendedores
+{
+
+    public static boolean cadastrado (int id) throws Exception
+    {
+        boolean retorno = false;
+
+        try
+        {
+            String sql;
+
+            sql = "SELECT * " +
+                    "FROM VENDEDORES " +
+                    "WHERE id = ?";
+
+            BDSQLServer.COMANDO.prepareStatement (sql);
+
+            BDSQLServer.COMANDO.setInt (1, id);
+
+            MeuResultSet resultado = (MeuResultSet)BDSQLServer.COMANDO.executeQuery ();
+
+            retorno = resultado.first(); // pode-se usar resultado.last() ou resultado.next() ou resultado.previous() ou resultado.absotule(numeroDaLinha)
+        }
+        catch (SQLException erro)
+        {
+            throw new Exception ("Erro ao procurar vendedor");
+        }
+
+        return retorno;
+    }
+
+    public static void incluir (Vendedor vendedor) throws Exception
+    {
+        if (vendedor==null)
+            throw new Exception ("Vendedor nao fornecido");
+
+        try
+        {
+            String sql;
+
+            sql = "INSERT INTO VENDEDORES " +
+                    "(NOME,EMAIL,TELEFONE,CPF,PRODUTO,CEP,NUMERO,COMPLEMENTO) " +
+                    "VALUES " +
+                    "(?,?,?,?,?,?,?,?)";
+
+            BDSQLServer.COMANDO.prepareStatement (sql);
+
+            BDSQLServer.COMANDO.setString (1, vendedor.getNome());
+            BDSQLServer.COMANDO.setString (2, vendedor.getEmail());
+            BDSQLServer.COMANDO.setString (3, vendedor.getTelefone());
+            BDSQLServer.COMANDO.setString (4, vendedor.getCpf());
+            BDSQLServer.COMANDO.setString (5, vendedor.getProduto());
+            BDSQLServer.COMANDO.setInt  (6, vendedor.getCep());
+            BDSQLServer.COMANDO.setShort  (7, vendedor.getNumero());
+            BDSQLServer.COMANDO.setString (8, vendedor.getComplemento());
+
+
+            BDSQLServer.COMANDO.executeUpdate ();
+            BDSQLServer.COMANDO.commit        ();
+        }
+        catch (SQLException erro)
+        {
+            //bd.BDSQLServer.COMANDO.rollback ();
+            throw new Exception ("Erro ao inserir vendedor");
+        }
+    }
+
+    public static void excluir (int id) throws Exception
+    {
+        if (cadastrado(id))
+            throw new Exception ("Nao cadastrado");
+
+        try
+        {
+            String sql;
+
+            sql = "DELETE FROM FORNECEDOR " +
+                    "WHERE ID=?";
+
+            BDSQLServer.COMANDO.prepareStatement (sql);
+
+
+            BDSQLServer.COMANDO.setInt (1, id);
+
+            BDSQLServer.COMANDO.executeUpdate ();
+            BDSQLServer.COMANDO.commit        ();
+        }
+        catch (SQLException erro)
+        {
+            throw new Exception ("Erro ao excluir vendedor");
+        }
+    }
+
+    public static void alterar (Vendedor vendedor) throws Exception
+    {
+        if (vendedor==null)
+            throw new Exception ("Fornecedor nao fornecido");
+
+        if (cadastrado(vendedor.getId()))
+            throw new Exception ("Nao cadastrado");
+
+        try
+        {
+            String sql;
+
+            sql = "UPDATE VENDEDORES " +
+                    "SET NOME=?, " +
+                    "EMAIL=?, " +
+                    "TELEFONE=?, " +
+                    "CNPJ=? " +
+                    "STATUSFORNECEDOR=? " +
+                    "CEP=? " +
+                    "NUMERO=? " +
+                    "COMPLEMENTO=? " +
+                    "WHERE ID = ?";
+
+            BDSQLServer.COMANDO.prepareStatement (sql);
+
+            BDSQLServer.COMANDO.setString (1, vendedor.getNome());
+            BDSQLServer.COMANDO.setString (2, vendedor.getEmail());
+            BDSQLServer.COMANDO.setString (3, vendedor.getTelefone());
+            BDSQLServer.COMANDO.setString (4, vendedor.getCpf());
+            BDSQLServer.COMANDO.setString (5, vendedor.getProduto());
+            BDSQLServer.COMANDO.setInt  (6, vendedor.getCep());
+            BDSQLServer.COMANDO.setShort  (7, vendedor.getNumero());
+            BDSQLServer.COMANDO.setString (8, vendedor.getComplemento());
+            BDSQLServer.COMANDO.setInt    (9, vendedor.getId());
+
+            BDSQLServer.COMANDO.executeUpdate ();
+            BDSQLServer.COMANDO.commit        ();
+        }
+        catch (SQLException erro)
+        {
+            throw new Exception ("Erro ao atualizar dados do vendedor");
+        }
+    }
+
+    public static Vendedor getVendedor (int id) throws Exception
+    {
+        Vendedor vendedor = null;
+
+        try
+        {
+            String sql;
+
+            sql = "SELECT * " +
+                    "FROM VENDEDORES " +
+                    "WHERE ID = ?";
+
+            BDSQLServer.COMANDO.prepareStatement (sql);
+
+            BDSQLServer.COMANDO.setInt (1, id);
+
+            MeuResultSet resultado = (MeuResultSet)BDSQLServer.COMANDO.executeQuery ();
+
+            if (!resultado.first())
+                throw new Exception ("Nao cadastrado");
+
+            vendedor = new Vendedor(resultado.getInt("ID"),
+                                        resultado.getInt("CEP"),
+                                        resultado.getShort("NUMERO"),
+                                        resultado.getString("NOME"),
+                                        resultado.getString("EMAIL"),
+                                        resultado.getString("TELEFONE"),
+                                        resultado.getString("CPF"),
+                                        resultado.getString("COMPLEMENTO"),
+                                        resultado.getString("PRODUTO"));
+        }
+        catch (SQLException erro)
+        {
+            throw new Exception ("Erro ao procurar vendedor");
+        }
+
+        return vendedor;
+    }
+
+    public static Vendedor getVendedor (String cpf) throws Exception
+    {
+        Vendedor vendedor = null;
+
+        try
+        {
+            String sql;
+
+            sql = "SELECT * " +
+                    "FROM VENDEDORES " +
+                    "WHERE CPF = ?";
+
+            BDSQLServer.COMANDO.prepareStatement (sql);
+
+            BDSQLServer.COMANDO.setString (1, cpf);
+
+            MeuResultSet resultado = (MeuResultSet)BDSQLServer.COMANDO.executeQuery ();
+
+            if (!resultado.first())
+                throw new Exception ("Nao cadastrado");
+
+            vendedor = new Vendedor(resultado.getInt("ID"),
+                    resultado.getInt("CEP"),
+                    resultado.getShort("NUMERO"),
+                    resultado.getString("NOME"),
+                    resultado.getString("EMAIL"),
+                    resultado.getString("TELEFONE"),
+                    resultado.getString("CPF"),
+                    resultado.getString("COMPLEMENTO"),
+                    resultado.getString("PRODUTO"));
+        }
+        catch (SQLException erro)
+        {
+            throw new Exception ("Erro ao procurar vendedor");
+        }
+
+        return vendedor;
+    }
+
+    public static MeuResultSet getVendedores () throws Exception
+    {
+        MeuResultSet resultado = null;
+
+        try
+        {
+            String sql;
+
+            sql = "SELECT * " +
+                    "FROM VENDEDORES";
+
+            BDSQLServer.COMANDO.prepareStatement (sql);
+
+            resultado = (MeuResultSet)BDSQLServer.COMANDO.executeQuery ();
+        }
+        catch (SQLException erro)
+        {
+            throw new Exception ("Erro ao recuperar vendedor");
+        }
+
+        return resultado;
+    }
+}
---
+++ Projeto I - AOS/src/bd/dbos/Vendedor.java
@@ -0,0 +1,214 @@
+package bd.dbos;
+
+import java.util.Objects;
+
+public class Vendedor implements Cloneable
+{
+    private int id;
+    private int cep;
+    private short numero;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String cpf;
+    private String complemento;
+    private String produto;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) throws Exception
+    {
+        if (id < 0) throw new Exception("Id inválido");
+        this.id = id;
+    }
+
+    public int getCep() {
+        return cep;
+    }
+
+    public void setCep(int cep) throws Exception
+    {
+        if (cep < 10000000) throw new Exception("Cep inválido");
+        this.cep = cep;
+    }
+
+    public short getNumero() {
+        return numero;
+    }
+
+    public void setNumero(short numero) throws Exception
+    {
+        if (numero < 0) throw new Exception("Número inválido");
+        this.numero = numero;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) throws Exception
+    {
+        if (nome.equals("") || nome == null) throw new Exception("Nome inválido");
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) throws Exception
+    {
+        if (email.equals("") || email == null) throw new Exception("Email inválido");
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) throws Exception
+    {
+        if (telefone.equals("") || telefone == null) throw new Exception("Telefone inválido");
+        this.telefone = telefone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) throws Exception
+    {
+        if (cpf.equals("") || cpf == null) throw new Exception("CPF inválido");
+        this.cpf = cpf;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getProduto() {
+        return produto;
+    }
+
+    public void setProduto(String produto) throws Exception
+    {
+        if (produto.equals("") || produto == null) throw new Exception("Produto inválido");
+        this.produto = produto;
+    }
+
+    public Vendedor(int    id  , int  cep        , short  numero  ,
+                      String nome, String email      , String telefone,
+                      String cpf, String complemento , String produto) throws Exception {
+        this.setId(id);
+        this.setCep(cep);
+        this.setNumero(numero);
+        this.setNome(nome);
+        this.setEmail(email);
+        this.setTelefone(telefone);
+        this.setCpf(cpf);
+        this.setComplemento(complemento);
+        this.setProduto(produto);
+    }
+
+    public String toString() {
+        String ret = "";
+
+        ret = "Vendedor{" +
+                "id=" + id +
+                ", cep=" + cep +
+                ", numero=" + numero +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", complemento='" + complemento + '\'' +
+                ", produto=" +produto+
+                '}';
+
+        return ret;
+    }
+
+    public boolean equals(Object obj)
+    {
+        if (this == obj) return true;
+
+        if (obj == null) return false;
+
+        if (!(obj instanceof Vendedor)) return false;
+
+        Vendedor forn = (Vendedor) obj;
+
+        if(getId() != forn.getId()) return false;
+
+        if (getCep() != forn.getCep()) return false;
+
+        if (getNumero() != forn.getNumero()) return false;
+
+        if (getProduto() != forn.getProduto()) return false;
+
+        if (!getNome().equals(forn.getNome())) return false;
+
+        if (!getEmail().equals(forn.getEmail())) return false;
+
+        if (!getTelefone().equals(forn.getTelefone())) return false;
+
+        if (!getCpf().equals(forn.getCpf())) return false;
+
+        if (!getComplemento().equals(forn.getComplemento())) return false;
+
+        return true;
+    }
+
+    public int hashCode()
+    {
+        int ret = 0;
+
+        ret =  Objects.hash(getId(),
+                            getCep(),
+                            getNumero(),
+                            getNome(),
+                            getEmail(),
+                            getTelefone(),
+                            getCpf(),
+                            getComplemento(),
+                            getProduto());
+
+        return ret;
+    }
+
+    public Object clone()
+    {
+        Vendedor ret = null;
+
+        try
+        {
+            ret = new Vendedor(this);
+        }
+        catch (Exception err){}
+
+        return ret;
+    }
+
+    public Vendedor (Vendedor modelo) throws Exception
+    {
+        if(modelo == null)
+            throw new Exception("Modelo para copia vazio");
+
+        this.id = modelo.id;
+        this.cep = modelo.cep;
+        this.cpf = modelo.cpf;
+        this.complemento = modelo.complemento;
+        this.email = modelo.email;
+        this.nome = modelo.nome;
+        this.numero = modelo.numero;
+        this.produto = modelo.produto;
+        this.telefone = modelo.telefone;
+    }
+}
